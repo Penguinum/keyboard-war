3e24ba34e74fe21e4308f3923367739e70b4b429
Penguinum <penguinum@gmail.com>
2017-07-23 21:57:29 +0300

    Update build/win32

--- build/win32/build.sh
+++ build/win32/build.sh
@@ -6,12 +6,13 @@ then
   wget https://bitbucket.org/rude/love/downloads/love-0.10.2-win32.zip
 fi
 
-if [ ! -e "keyboardwars" ]
-then
-  unzip love-0.10.2-win32.zip
-  mv love-0.10.2-win32 keyboardwars
-fi
+rm -rf keyboardwars
 
-cd ../../ && zip -9 -r KeyboardWars.love . -x "build/*" ".git/*" "*/.git" "*/.git/*"
+unzip love-0.10.2-win32.zip
+mv love-0.10.2-win32 keyboardwars
+
+cd ../../ && zip -9 -r KeyboardWars.love . -x "build/*" ".gitmodules" ".git/*" "*/.git" "*/.git/*"
 cd - && mv ../../KeyboardWars.love .
 cat keyboardwars/love.exe KeyboardWars.love > keyboardwars/KeyboardWars.exe
+cd keyboardwars && rm love.exe lovec.exe readme.txt changes.txt
+cp ../../../README.md .
